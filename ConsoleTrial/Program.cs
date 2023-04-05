21072dbe0358feef5260d41e341fba61c25d79f9
Muratcan Kara <muratfivem@gmail.com>
2023-04-05 19:31:11 +0300

    Add project files.

---
+++ ConsoleTrial/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleTrial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager1 = new CarManager(new InMemoryCarDal());
+            foreach (var c in carManager1.GetAll())
+            {
+                Console.WriteLine(c.Description);
+            }                                  
+        }
+    }
+}
